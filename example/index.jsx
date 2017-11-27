2824e6a97bfcef1233cff9ea06e4c35c147b4981
Ernie Yang <ernieyang09@hotmail.com>
2017-11-27 15:46:49 +0800

    add example map

--- example/index.jsx
+++ example/index.jsx
@@ -1,5 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Map, TileLayer } from 'react-leaflet';
+
+const position = [51.505, -0.09];
+
+class App extends Component {
+  render () {
+    return (
+      <Map center={position} zoom={13}>
+        <TileLayer
+          url="https://cartodb-basemaps-a.global.ssl.fastly.net/rastertiles/voyager_labels_under/{z}/{x}/{y}.png"
+        />
+      </Map>
+    );
+  }
+}
 
 
 ReactDOM.render(
